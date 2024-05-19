4f864a52edd08fa29426d25a17c4122ec70fa72a
hozayves <muhozayves2014@gmail.com>
2024-05-20 00:02:44 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui'),],
+}
